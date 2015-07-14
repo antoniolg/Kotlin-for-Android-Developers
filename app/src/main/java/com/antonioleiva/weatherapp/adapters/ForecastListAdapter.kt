eb443f096423c6c4edfb7cb2c0cdbd252346feed
Antonio Leiva <antonioleivag@gmail.com>
2015-07-14 23:12:08 +0200

    Chapter 5: Writing your first class

---
+++ app/src/main/java/com/antonioleiva/weatherapp/adapters/ForecastListAdapter.kt
@@ -0,0 +1,20 @@
+package com.antonioleiva.weatherapp.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.ViewGroup
+import android.widget.TextView
+
+public class ForecastListAdapter(val items: List<String>) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ForecastListAdapter.ViewHolder? {
+        return ViewHolder(TextView(parent.context))
+    }
+
+    override fun onBindViewHolder(holder: ForecastListAdapter.ViewHolder, position: Int) {
+        holder.textView.text = items.get(position)
+    }
+
+    override fun getItemCount(): Int = items.size()
+
+    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+}
