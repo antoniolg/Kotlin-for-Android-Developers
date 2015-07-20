47775d9ad16d1b4bdaa4154ab1a92e7f352f49e5
Antonio Leiva <antonioleivag@gmail.com>
2015-07-20 19:36:04 +0200

    Chapter 8: Retrieving data from API

--- app/src/main/java/com/antonioleiva/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/activities/MainActivity.kt
@@ -1,12 +1,16 @@
-package com.antonioleiva.weatherapp.activities
+package com.antonioleiva.weatherapp.ui.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.antonioleiva.weatherapp.R
-import com.antonioleiva.weatherapp.adapters.ForecastListAdapter
+import com.antonioleiva.weatherapp.data.Request
+import com.antonioleiva.weatherapp.ui.adapters.ForecastListAdapter
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -27,5 +31,13 @@ class MainActivity : AppCompatActivity() {
         val forecastList = find<RecyclerView>(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastListAdapter(items)
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
+        doAsync {
+            Request(url).run()
+            uiThread { longToast("Request performed") }
+        }
     }
 }
