321ffe1f109e98e454c98f7f104d7bbf1a6a0d33
Antonio Leiva <antonioleivag@gmail.com>
2015-07-31 10:07:01 +0200

    Chapter 10: Parsing data

--- app/src/main/java/com/antonioleiva/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -1,20 +1,25 @@
 package com.antonioleiva.weatherapp.ui.adapters
 
+import android.annotation.SuppressLint
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.TextView
+import com.antonioleiva.weatherapp.domain.model.ForecastList
 
-class ForecastListAdapter(private val items: List<String>)
+class ForecastListAdapter(private val weekForecast: ForecastList)
     : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
             ViewHolder(TextView(parent.context))
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.textView.text = items[position]
+        with(weekForecast.dailyForecast[position]) {
+            holder.textView.text = "$date - $description - $high/$low"
+        }
     }
 
-    override fun getItemCount(): Int = items.size
+    override fun getItemCount(): Int = weekForecast.dailyForecast.size
 
     class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 }
