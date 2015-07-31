e29d752a4edda431d7ca25d480d82888a3a97950
Antonio Leiva <antonioleivag@gmail.com>
2015-07-31 10:07:01 +0200

    Chapter 10: Parsing data

---
+++ app/src/main/java/com/antonioleiva/weatherapp/data/ForecastRequest.kt
@@ -0,0 +1,18 @@
+package com.antonioleiva.weatherapp.data
+
+import com.google.gson.Gson
+import java.net.URL
+
+class ForecastRequest(val zipCode: String) {
+
+    companion object {
+        private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
+        private val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=7"
+        private val COMPLETE_URL = "$URL&APPID=$APP_ID&q="
+    }
+
+    fun execute(): ForecastResult {
+        val forecastJsonStr = URL(COMPLETE_URL + zipCode).readText()
+        return Gson().fromJson(forecastJsonStr, ForecastResult::class.java)
+    }
+}
---
+++ app/src/main/java/com/antonioleiva/weatherapp/domain/mappers/ForecastDataMapper.kt
@@ -0,0 +1,33 @@
+package com.antonioleiva.weatherapp.domain.mappers
+
+import com.antonioleiva.weatherapp.data.Forecast
+import com.antonioleiva.weatherapp.data.ForecastResult
+import com.antonioleiva.weatherapp.domain.model.ForecastList
+import java.text.DateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+import com.antonioleiva.weatherapp.domain.model.Forecast as ModelForecast
+
+class ForecastDataMapper {
+
+    fun convertFromDataModel(forecast: ForecastResult): ForecastList {
+        return ForecastList(forecast.city.name, forecast.city.country, convertForecastListToDomain(forecast.list))
+    }
+
+    private fun convertForecastListToDomain(list: List<Forecast>): List<ModelForecast> {
+        return list.mapIndexed { i, forecast ->
+            val dt = Calendar.getInstance().timeInMillis + TimeUnit.DAYS.toMillis(i.toLong())
+            convertForecastItemToDomain(forecast.copy(dt = dt))
+        }
+    }
+
+    private fun convertForecastItemToDomain(forecast: Forecast): ModelForecast {
+        return ModelForecast(convertDate(forecast.dt), forecast.weather[0].description,
+                forecast.temp.max.toInt(), forecast.temp.min.toInt())
+    }
+
+    private fun convertDate(date: Long): String {
+        val df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
+        return df.format(date)
+    }
+}
