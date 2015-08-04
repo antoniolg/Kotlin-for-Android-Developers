583bb10f2394c476a1e504617a5c37ae3856bbfc
Antonio Leiva <antonioleivag@gmail.com>
2015-08-04 08:10:45 +0200

    Chapter 11: Operator overloading

--- app/src/main/java/com/antonioleiva/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/domain/model/DomainClasses.kt
@@ -1,4 +1,11 @@
 package com.antonioleiva.weatherapp.domain.model
 
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>) {
+
+    val size: Int
+        get() = dailyForecast.size
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+}
+
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
