fc1271ab5a4a14ddb12467693765a4827d1cebf5
Antonio Leiva <antonioleivag@gmail.com>
2015-08-05 08:44:21 +0200

    Chapter 12: Making the forecast list clickable

--- app/src/main/java/com/antonioleiva/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/domain/model/DomainClasses.kt
@@ -5,4 +5,5 @@ data class ForecastList(val city: String, val country: String, val dailyForecast
     fun size(): Int = dailyForecast.size
 }
 
-data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
+data class Forecast(val date: String, val description: String, val high: Int, val low: Int,
+                    val iconUrl: String)
--- app/src/main/java/com/antonioleiva/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -1,23 +1,62 @@
 package com.antonioleiva.weatherapp.ui.adapters
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
+import com.antonioleiva.weatherapp.R
+import com.antonioleiva.weatherapp.domain.model.Forecast
 import com.antonioleiva.weatherapp.domain.model.ForecastList
+import com.antonioleiva.weatherapp.ui.utils.ctx
+import com.squareup.picasso.Picasso
+import org.jetbrains.anko.find
 
-class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+class ForecastListAdapter(val weekForecast: ForecastList,
+                                 val itemClick: ForecastListAdapter.OnItemClickListener) :
+        RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(TextView(parent.context))
+        val view = LayoutInflater.from(parent.ctx).inflate(R.layout.item_forecast, parent, false)
+        return ViewHolder(view, itemClick)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        with(weekForecast[position]) {
-            holder.textView.text = "$date - $description - $high/$low"
-        }
+        holder.bindForecast(weekForecast[position])
     }
 
     override fun getItemCount(): Int = weekForecast.size()
 
-    class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
+    class ViewHolder(view: View, val itemClick: OnItemClickListener) : RecyclerView.ViewHolder(view) {
+
+        private val iconView: ImageView
+        private val dateView: TextView
+        private val descriptionView: TextView
+        private val maxTemperatureView: TextView
+        private val minTemperatureView: TextView
+
+        init {
+            iconView = view.find(R.id.icon)
+            dateView = view.find(R.id.date)
+            descriptionView = view.find(R.id.description)
+            maxTemperatureView = view.find(R.id.maxTemperature)
+            minTemperatureView = view.find(R.id.minTemperature)
+        }
+
+        fun bindForecast(forecast: Forecast) {
+            with(forecast) {
+                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
+                dateView.text = date
+                descriptionView.text = description
+                maxTemperatureView.text = "${high.toString()}º"
+                minTemperatureView.text = "${low.toString()}º"
+                itemView.setOnClickListener { itemClick(this) }
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        operator fun invoke(forecast: Forecast)
+    }
 }
