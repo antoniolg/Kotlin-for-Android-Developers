7905dfecef651ff76cdf1285c85945e126c6cabe
Antonio Leiva <antonioleivag@gmail.com>
2015-08-05 08:44:21 +0200

    Chapter 19: Saving and requesting data from database

---
+++ app/src/main/java/com/antonioleiva/weatherapp/data/db/ForecastDb.kt
@@ -0,0 +1,39 @@
+package com.antonioleiva.weatherapp.data.db
+
+import com.antonioleiva.weatherapp.domain.model.ForecastList
+import com.antonioleiva.weatherapp.extensions.clear
+import com.antonioleiva.weatherapp.extensions.parseList
+import com.antonioleiva.weatherapp.extensions.parseOpt
+import com.antonioleiva.weatherapp.extensions.toVarargArray
+import org.jetbrains.anko.db.insert
+import org.jetbrains.anko.db.select
+import java.util.*
+
+class ForecastDb(val forecastDbHelper: ForecastDbHelper = ForecastDbHelper.instance,
+                 val dataMapper: DbDataMapper = DbDataMapper()) {
+
+    fun requestForecastByZipCode(zipCode: Long, date: Long) = forecastDbHelper.use {
+
+        val dailyRequest = "${DayForecastTable.CITY_ID} = ? AND ${DayForecastTable.DATE} >= ?"
+        val dailyForecast = select(DayForecastTable.NAME)
+                .whereSimple(dailyRequest, zipCode.toString(), date.toString())
+                .parseList { DayForecast(HashMap(it)) }
+
+        val city = select(CityForecastTable.NAME)
+                .whereSimple("${CityForecastTable.ID} = ?", zipCode.toString())
+                .parseOpt { CityForecast(HashMap(it), dailyForecast) }
+
+        if (city != null) dataMapper.convertToDomain(city) else null
+    }
+
+    fun saveForecast(forecast: ForecastList) = forecastDbHelper.use {
+
+        clear(CityForecastTable.NAME)
+        clear(DayForecastTable.NAME)
+
+        with(dataMapper.convertFromDomain(forecast)) {
+            insert(CityForecastTable.NAME, *map.toVarargArray())
+            dailyForecast.forEach { insert(DayForecastTable.NAME, *it.map.toVarargArray()) }
+        }
+    }
+}
