4b7f96aa480ade17b3a509683bb833ab48c4996a
Antonio Leiva <antonioleivag@gmail.com>
2015-08-05 08:44:21 +0200

    Chapter 17: Creating an SQLiteOpenHelper

---
+++ app/src/main/java/com/antonioleiva/weatherapp/data/db/Tables.kt
@@ -0,0 +1,19 @@
+package com.antonioleiva.weatherapp.data.db
+
+object CityForecastTable {
+    val NAME = "CityForecast"
+    val ID = "_id"
+    val CITY = "city"
+    val COUNTRY = "country"
+}
+
+object DayForecastTable {
+    val NAME = "DayForecast"
+    val ID = "_id"
+    val DATE = "date"
+    val DESCRIPTION = "description"
+    val HIGH = "high"
+    val LOW = "low"
+    val ICON_URL = "iconUrl"
+    val CITY_ID = "cityId"
+}
