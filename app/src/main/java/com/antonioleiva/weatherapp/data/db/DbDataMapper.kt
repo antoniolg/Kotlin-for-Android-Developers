c72118e411a9462a37370c1f53a31fc63fe6748b
Antonio Leiva <antonioleivag@gmail.com>
2015-08-05 08:44:21 +0200

    Chapter 19: Saving and requesting data from database

---
+++ app/src/main/java/com/antonioleiva/weatherapp/data/db/DbDataMapper.kt
@@ -0,0 +1,25 @@
+package com.antonioleiva.weatherapp.data.db
+
+import com.antonioleiva.weatherapp.domain.model.Forecast
+import com.antonioleiva.weatherapp.domain.model.ForecastList
+
+class DbDataMapper {
+
+    fun convertFromDomain(forecast: ForecastList) = with(forecast) {
+        val daily = dailyForecast.map { convertDayFromDomain(id, it) }
+        CityForecast(id, city, country, daily)
+    }
+
+    private fun convertDayFromDomain(cityId: Long, forecast: Forecast) = with(forecast) {
+        DayForecast(date, description, high, low, iconUrl, cityId)
+    }
+
+    fun convertToDomain(forecast: CityForecast) = with(forecast) {
+        val daily = dailyForecast.map { convertDayToDomain(it) }
+        ForecastList(_id, city, country, daily)
+    }
+
+    private fun convertDayToDomain(dayForecast: DayForecast) = with(dayForecast) {
+        Forecast(date, description, high, low, iconUrl)
+    }
+}
--- app/src/main/java/com/antonioleiva/weatherapp/domain/mappers/ForecastDataMapper.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/domain/mappers/ForecastDataMapper.kt
@@ -1,17 +1,17 @@
 package com.antonioleiva.weatherapp.domain.mappers
 
-import com.antonioleiva.weatherapp.data.Forecast
-import com.antonioleiva.weatherapp.data.ForecastResult
+import com.antonioleiva.weatherapp.data.server.Forecast
+import com.antonioleiva.weatherapp.data.server.ForecastResult
 import com.antonioleiva.weatherapp.domain.model.ForecastList
-import java.text.DateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 import com.antonioleiva.weatherapp.domain.model.Forecast as ModelForecast
 
 class ForecastDataMapper {
 
-    fun convertFromDataModel(forecast: ForecastResult): ForecastList =
-            ForecastList(forecast.city.name, forecast.city.country, convertForecastListToDomain(forecast.list))
+    fun convertFromDataModel(zipCode: Long, forecast: ForecastResult) = with(forecast) {
+        ForecastList(zipCode, city.name, city.country, convertForecastListToDomain(list))
+    }
 
     private fun convertForecastListToDomain(list: List<Forecast>): List<ModelForecast> {
         return list.mapIndexed { i, forecast ->
@@ -20,14 +20,9 @@ class ForecastDataMapper {
         }
     }
 
-    private fun convertForecastItemToDomain(forecast: Forecast): ModelForecast {
-        return ModelForecast(convertDate(forecast.dt), forecast.weather[0].description,
-                forecast.temp.max.toInt(), forecast.temp.min.toInt(), generateIconUrl(forecast.weather[0].icon))
-    }
-
-    private fun convertDate(date: Long): String {
-        val df = DateFormat.getDateInstance(DateFormat.MEDIUM, Locale.getDefault())
-        return df.format(date)
+    private fun convertForecastItemToDomain(forecast: Forecast) = with(forecast) {
+        ModelForecast(dt, weather[0].description, temp.max.toInt(), temp.min.toInt(),
+                generateIconUrl(weather[0].icon))
     }
 
     private fun generateIconUrl(iconCode: String) = "http://openweathermap.org/img/w/$iconCode.png"
