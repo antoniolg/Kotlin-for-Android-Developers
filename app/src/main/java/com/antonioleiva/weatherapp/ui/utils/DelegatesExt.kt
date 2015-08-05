e755ad8b4622a8f38b196024e1d956dfd4d800c0
Antonio Leiva <antonioleivag@gmail.com>
2015-08-05 08:44:21 +0200

    Chapter 16: Application Singleton and Delegated Properties

---
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/utils/DelegatesExt.kt
@@ -0,0 +1,20 @@
+package com.antonioleiva.weatherapp.ui.utils
+
+import kotlin.reflect.KProperty
+
+object DelegatesExt {
+    fun <T> notNullSingleValue() = NotNullSingleValueVar<T>()
+}
+
+class NotNullSingleValueVar<T> {
+
+    private var value: T? = null
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T =
+            value ?: throw IllegalStateException("${property.name} not initialized")
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.value = if (this.value == null) value
+        else throw IllegalStateException("${property.name} already initialized")
+    }
+}
