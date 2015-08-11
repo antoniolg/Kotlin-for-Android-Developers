07e49267fc1521ec97f8a86d46a7b85572db01a6
Antonio Leiva <antonioleivag@gmail.com>
2015-08-11 08:32:34 +0200

    Chapter 15: Kotlin Android Extensions

--- app/src/main/java/com/antonioleiva/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/activities/MainActivity.kt
@@ -3,12 +3,11 @@ package com.antonioleiva.weatherapp.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.antonioleiva.weatherapp.R
 import com.antonioleiva.weatherapp.domain.commands.RequestForecastCommand
 import com.antonioleiva.weatherapp.ui.adapters.ForecastListAdapter
+import kotlinx.android.synthetic.main.activity_main.forecastList
 import org.jetbrains.anko.async
-import org.jetbrains.anko.find
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
@@ -18,8 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val forecastList: RecyclerView = find(R.id.forecast_list)
         forecastList.layoutManager = LinearLayoutManager(this)
 
         async() {
--- app/src/main/java/com/antonioleiva/weatherapp/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/adapters/ForecastListAdapter.kt
@@ -4,14 +4,12 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import com.antonioleiva.weatherapp.R
 import com.antonioleiva.weatherapp.domain.model.Forecast
 import com.antonioleiva.weatherapp.domain.model.ForecastList
 import com.antonioleiva.weatherapp.ui.utils.ctx
 import com.squareup.picasso.Picasso
-import org.jetbrains.anko.find
+import kotlinx.android.synthetic.main.item_forecast.view.*
 
 class ForecastListAdapter(val weekForecast: ForecastList, val itemClick: (Forecast) -> Unit) :
         RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
@@ -29,27 +27,13 @@ class ForecastListAdapter(val weekForecast: ForecastList, val itemClick: (Foreca
 
     class ViewHolder(view: View, val itemClick: (Forecast) -> Unit) : RecyclerView.ViewHolder(view) {
 
-        private val iconView: ImageView
-        private val dateView: TextView
-        private val descriptionView: TextView
-        private val maxTemperatureView: TextView
-        private val minTemperatureView: TextView
-
-        init {
-            iconView = view.find(R.id.icon)
-            dateView = view.find(R.id.date)
-            descriptionView = view.find(R.id.description)
-            maxTemperatureView = view.find(R.id.maxTemperature)
-            minTemperatureView = view.find(R.id.minTemperature)
-        }
-
         fun bindForecast(forecast: Forecast) {
             with(forecast) {
-                Picasso.with(itemView.ctx).load(iconUrl).into(iconView)
-                dateView.text = date
-                descriptionView.text = description
-                maxTemperatureView.text = "${high.toString()}º"
-                minTemperatureView.text = "${low.toString()}º"
+                Picasso.with(itemView.ctx).load(iconUrl).into(itemView.icon)
+                itemView.date.text = date
+                itemView.description.text = description
+                itemView.maxTemperature.text = "${high.toString()}º"
+                itemView.minTemperature.text = "${low.toString()}º"
                 itemView.setOnClickListener { itemClick(this) }
             }
         }
