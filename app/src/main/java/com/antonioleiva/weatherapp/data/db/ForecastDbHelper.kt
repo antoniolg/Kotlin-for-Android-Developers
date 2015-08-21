1b038e0e5c3bdf742ebb9222d692a567e7ad6c99
Antonio Leiva <antonioleivag@gmail.com>
2015-08-21 08:11:15 +0200

    Chapter 17: Creating an SQLiteOpenHelper

---
+++ app/src/main/java/com/antonioleiva/weatherapp/data/db/ForecastDbHelper.kt
@@ -0,0 +1,39 @@
+package com.antonioleiva.weatherapp.data.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import com.antonioleiva.weatherapp.ui.App
+import org.jetbrains.anko.db.*
+
+class ForecastDbHelper(ctx: Context = App.instance) : ManagedSQLiteOpenHelper(ctx,
+        ForecastDbHelper.DB_NAME, null, ForecastDbHelper.DB_VERSION) {
+
+    companion object {
+        val DB_NAME = "forecast.db"
+        val DB_VERSION = 1
+        val instance: ForecastDbHelper by lazy { ForecastDbHelper() }
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.createTable(CityForecastTable.NAME, true,
+                CityForecastTable.ID to INTEGER + PRIMARY_KEY,
+                CityForecastTable.CITY to TEXT,
+                CityForecastTable.COUNTRY to TEXT)
+
+        db.createTable(DayForecastTable.NAME, true,
+                DayForecastTable.ID to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
+                DayForecastTable.DATE to INTEGER,
+                DayForecastTable.DESCRIPTION to TEXT,
+                DayForecastTable.HIGH to INTEGER,
+                DayForecastTable.LOW to INTEGER,
+                DayForecastTable.ICON_URL to TEXT,
+                DayForecastTable.CITY_ID to INTEGER)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.dropTable(CityForecastTable.NAME, true)
+        db.dropTable(DayForecastTable.NAME, true)
+        onCreate(db)
+    }
+}
+
