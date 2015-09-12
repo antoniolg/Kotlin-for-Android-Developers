6437b2a0bb248863debcaad36632cd1367ec00c9
Antonio Leiva <antonioleivag@gmail.com>
2015-09-12 20:55:17 +0200

    Chapter 23: Creating a Detail Activity

--- app/src/main/java/com/antonioleiva/weatherapp/extensions/ViewExtensions.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/extensions/ViewExtensions.kt
@@ -2,6 +2,11 @@ package com.antonioleiva.weatherapp.extensions
 
 import android.content.Context
 import android.view.View
+import android.widget.TextView
 
 val View.ctx: Context
     get() = context
+
+var TextView.textColor: Int
+    get() = currentTextColor
+    set(v) = setTextColor(v)
