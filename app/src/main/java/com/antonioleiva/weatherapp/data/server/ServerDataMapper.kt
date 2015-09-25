a0f68bb6a5d3251aabcd5354b8976cb9699cd387
Antonio Leiva <antonioleivag@gmail.com>
2015-09-25 08:23:18 +0200

    Chapter 21: Creating the business logic to data access

--- app/src/main/java/com/antonioleiva/weatherapp/data/server/ServerDataMapper.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/data/server/ServerDataMapper.kt
@@ -1,15 +1,13 @@
-package com.antonioleiva.weatherapp.domain.mappers
+package com.antonioleiva.weatherapp.data.server
 
-import com.antonioleiva.weatherapp.data.server.Forecast
-import com.antonioleiva.weatherapp.data.server.ForecastResult
 import com.antonioleiva.weatherapp.domain.model.ForecastList
 import java.util.*
 import java.util.concurrent.TimeUnit
 import com.antonioleiva.weatherapp.domain.model.Forecast as ModelForecast
 
-class ForecastDataMapper {
+class ServerDataMapper {
 
-    fun convertFromDataModel(zipCode: Long, forecast: ForecastResult) = with(forecast) {
+    fun convertToDomain(zipCode: Long, forecast: ForecastResult) = with(forecast) {
         ForecastList(zipCode, city.name, city.country, convertForecastListToDomain(list))
     }
 
