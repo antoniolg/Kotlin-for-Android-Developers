8a055a60740f972de2ee33de21bb8adafe17c9e9
Antonio Leiva <antonioleivag@gmail.com>
2015-09-28 07:56:11 +0200

    Chapter 24: Interfaces and Delegation

--- app/src/main/java/com/antonioleiva/weatherapp/ui/activities/DetailActivity.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/activities/DetailActivity.kt
@@ -2,6 +2,7 @@ package com.antonioleiva.weatherapp.ui.activities;
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.widget.TextView
 import com.antonioleiva.weatherapp.R
 import com.antonioleiva.weatherapp.domain.commands.RequestDayForecastCommand
@@ -11,12 +12,12 @@ import com.antonioleiva.weatherapp.extensions.textColor
 import com.antonioleiva.weatherapp.extensions.toDateString
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail.*
-import org.jetbrains.anko.async
-import org.jetbrains.anko.ctx
-import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.*
 import java.text.DateFormat
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), ToolbarManager {
+
+    override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
 
     companion object {
         val ID = "DetailActivity:id"
@@ -26,8 +27,10 @@ class DetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail)
+        initToolbar()
 
-        title = intent.getStringExtra(CITY_NAME)
+        toolbarTitle = intent.getStringExtra(CITY_NAME)
+        enableHomeAsUp { onBackPressed() }
 
         async() {
             val result = RequestDayForecastCommand(intent.getLongExtra(ID, -1)).execute()
@@ -37,7 +40,7 @@ class DetailActivity : AppCompatActivity() {
 
     private fun bindForecast(forecast: Forecast) = with(forecast) {
         Picasso.with(ctx).load(iconUrl).into(icon)
-        supportActionBar?.subtitle = date.toDateString(DateFormat.FULL)
+        toolbar.subtitle = date.toDateString(DateFormat.FULL)
         weatherDescription.text = description
         bindWeather(high to maxTemperature, low to minTemperature)
     }
--- app/src/main/java/com/antonioleiva/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/activities/MainActivity.kt
@@ -3,21 +3,27 @@ package com.antonioleiva.weatherapp.ui.activities
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.Toolbar
 import com.antonioleiva.weatherapp.R
 import com.antonioleiva.weatherapp.domain.commands.RequestForecastCommand
 import com.antonioleiva.weatherapp.ui.adapters.ForecastListAdapter
 import kotlinx.android.synthetic.main.activity_main.forecastList
 import org.jetbrains.anko.async
+import org.jetbrains.anko.find
 import org.jetbrains.anko.startActivity
 import org.jetbrains.anko.uiThread
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), ToolbarManager {
+
+    override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initToolbar()
 
         forecastList.layoutManager = LinearLayoutManager(this)
+        attachToScroll(forecastList)
 
         async() {
             val result = RequestForecastCommand(94043).execute()
@@ -27,7 +33,7 @@ class MainActivity : AppCompatActivity() {
                             DetailActivity.CITY_NAME to result.city)
                 }
                 forecastList.adapter = adapter
-                title = "${result.city} (${result.country})"
+                toolbarTitle = "${result.city} (${result.country})"
             }
         }
     }
