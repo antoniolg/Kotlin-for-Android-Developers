837fe1d9f6a20b4ca1373593e4105b9ad1053b12
Antonio Leiva <antonioleivag@gmail.com>
2015-10-06 09:53:43 +0200

    Chapter 24: Interfaces and Delegation

--- app/src/main/java/com/antonioleiva/weatherapp/extensions/ViewExtensions.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/extensions/ViewExtensions.kt
@@ -10,3 +10,11 @@ val View.ctx: Context
 var TextView.textColor: Int
     get() = currentTextColor
     set(v) = setTextColor(v)
+
+fun View.slideExit() {
+    if (translationY == 0f) animate().translationY(-height.toFloat())
+}
+
+fun View.slideEnter() {
+    if (translationY < 0f) animate().translationY(0f)
+}
