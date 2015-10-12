989d43da96250457c21ba9013da348aa284b974f
Antonio Leiva <antonioleivag@gmail.com>
2015-10-12 10:53:54 +0200

    Include API KEY in requests

--- app/src/main/java/com/antonioleiva/weatherapp/data/server/ForecastByZipCodeRequest.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/data/server/ForecastByZipCodeRequest.kt
@@ -6,12 +6,13 @@ import java.net.URL
 class ForecastByZipCodeRequest(val zipCode: Long, val gson: Gson = Gson()) {
 
     companion object {
-        private val URL = "http://api.openweathermap.org/data/2.5/" +
-                "forecast/daily?mode=json&units=metric&cnt=7&q="
+        private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
+        private val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=7"
+        private val COMPLETE_URL = "$URL&APPID=$APP_ID&q="
     }
 
     fun execute(): ForecastResult {
-        val forecastJsonStr = URL(URL + zipCode).readText()
+        val forecastJsonStr = URL(COMPLETE_URL + zipCode).readText()
         return gson.fromJson(forecastJsonStr, ForecastResult::class.java)
     }
 }
