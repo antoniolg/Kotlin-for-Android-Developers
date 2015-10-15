f75d1c34bf7a53ef469447e668bbac9e79d5b390
Antonio Leiva <antonioleivag@gmail.com>
2015-10-16 00:31:32 +0200

    Chapter 26: Settings screen

--- app/src/main/java/com/antonioleiva/weatherapp/extensions/DelegatesExtensions.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/extensions/DelegatesExtensions.kt
@@ -1,11 +1,12 @@
 package com.antonioleiva.weatherapp.extensions
 
+import android.content.Context
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 object DelegatesExt {
-    fun <T> notNullSingleValue():
-            ReadWriteProperty<Any?, T> = NotNullSingleValueVar()
+    fun <T> notNullSingleValue(): ReadWriteProperty<Any?, T> = NotNullSingleValueVar()
+    fun preference(context: Context, name: String, default: Long) = Preference(context, name, default)
 }
 
 private class NotNullSingleValueVar<T>() : ReadWriteProperty<Any?, T> {
@@ -19,5 +20,43 @@ private class NotNullSingleValueVar<T>() : ReadWriteProperty<Any?, T> {
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
         this.value = if (this.value == null) value
         else throw IllegalStateException("${property.name} already initialized")
+    }
+}
+
+class Preference<T>(val context: Context, val name: String, val default: T) : ReadWriteProperty<Any?, T> {
+
+    val prefs by lazy { context.getSharedPreferences("default", Context.MODE_PRIVATE) }
+
+    override fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return findPreference(name, default)
+    }
+
+    override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        putPreference(name, value)
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    private fun <U> findPreference(name: String, default: U): U = with(prefs) {
+        val res: Any = when (default) {
+            is Long -> getLong(name, default)
+            is String -> getString(name, default)
+            is Int -> getInt(name, default)
+            is Boolean -> getBoolean(name, default)
+            is Float -> getFloat(name, default)
+            else -> throw IllegalArgumentException("This type can be saved into Preferences")
+        }
+
+        res as U
+    }
+
+    private fun <U> putPreference(name: String, value: U) = with(prefs.edit()) {
+        when (value) {
+            is Long -> putLong(name, value)
+            is String -> putString(name, value)
+            is Int -> putInt(name, value)
+            is Boolean -> putBoolean(name, value)
+            is Float -> putFloat(name, value)
+            else -> throw IllegalArgumentException("This type can be saved into Preferences")
+        }.apply()
     }
 }
--- app/src/main/java/com/antonioleiva/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/activities/MainActivity.kt
@@ -6,6 +6,7 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.Toolbar
 import com.antonioleiva.weatherapp.R
 import com.antonioleiva.weatherapp.domain.commands.RequestForecastCommand
+import com.antonioleiva.weatherapp.extensions.DelegatesExt
 import com.antonioleiva.weatherapp.ui.adapters.ForecastListAdapter
 import kotlinx.android.synthetic.main.activity_main.forecastList
 import org.jetbrains.anko.async
@@ -15,6 +16,8 @@ import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity(), ToolbarManager {
 
+    val zipCode: Long by DelegatesExt.preference(this, SettingsActivity.ZIP_CODE,
+            SettingsActivity.DEFAULT_ZIP)
     override val toolbar by lazy { find<Toolbar>(R.id.toolbar) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,17 +27,22 @@ class MainActivity : AppCompatActivity(), ToolbarManager {
 
         forecastList.layoutManager = LinearLayoutManager(this)
         attachToScroll(forecastList)
+    }
 
-        async() {
-            val result = RequestForecastCommand(94043).execute()
-            uiThread {
-                val adapter = ForecastListAdapter(result) {
-                    startActivity<DetailActivity>(DetailActivity.ID to it.id,
-                            DetailActivity.CITY_NAME to result.city)
-                }
-                forecastList.adapter = adapter
-                toolbarTitle = "${result.city} (${result.country})"
+    override fun onResume() {
+        super.onResume()
+        loadForecast()
+    }
+
+    private fun loadForecast() = async() {
+        val result = RequestForecastCommand(zipCode).execute()
+        uiThread {
+            val adapter = ForecastListAdapter(result) {
+                startActivity<DetailActivity>(DetailActivity.ID to it.id,
+                        DetailActivity.CITY_NAME to result.city)
             }
+            forecastList.adapter = adapter
+            toolbarTitle = "${result.city} (${result.country})"
         }
     }
 }
