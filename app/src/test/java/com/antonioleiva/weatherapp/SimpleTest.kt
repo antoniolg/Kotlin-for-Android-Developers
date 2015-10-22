1d826656e2f411ca399c9cfacf8fe9cfba245141
Antonio Leiva <antonioleivag@gmail.com>
2015-10-22 10:46:42 +0200

    Chapter 28: Testing your app

---
+++ app/src/test/java/com/antonioleiva/weatherapp/SimpleTest.kt
@@ -0,0 +1,11 @@
+package com.antonioleiva.weatherapp
+
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class SimpleTest {
+
+    @Test fun unitTestingWorks() {
+        assertTrue(true)
+    }
+}
---
+++ app/src/test/java/com/antonioleiva/weatherapp/domain/commands/RequestDayForecastCommandTest.kt
@@ -0,0 +1,19 @@
+package com.antonioleiva.weatherapp.domain.commands
+
+import com.antonioleiva.weatherapp.domain.datasource.ForecastProvider
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+
+class RequestDayForecastCommandTest {
+
+    @Test
+    fun testProviderIsCalled() {
+        val forecastProvider = mock(ForecastProvider::class.java)
+        val command = RequestDayForecastCommand(2, forecastProvider)
+
+        command.execute()
+
+        verify(forecastProvider).requestForecast(2)
+    }
+}
