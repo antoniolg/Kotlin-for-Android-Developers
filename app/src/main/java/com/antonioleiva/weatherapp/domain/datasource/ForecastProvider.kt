3671c1d7d9f2e86af3087739011f095e497bb625
Antonio Leiva <antonioleivag@gmail.com>
2015-10-22 10:46:42 +0200

    Chapter 28: Testing your app

--- app/src/main/java/com/antonioleiva/weatherapp/domain/datasource/ForecastProvider.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/domain/datasource/ForecastProvider.kt
@@ -10,7 +10,7 @@ class ForecastProvider(private val sources: List<ForecastDataSource> = ForecastP
 
     companion object {
         val DAY_IN_MILLIS = 1000 * 60 * 60 * 24
-        val SOURCES = listOf(ForecastDb(), ForecastServer())
+        val SOURCES by lazy { listOf(ForecastDb(), ForecastServer()) }
     }
 
     fun requestByZipCode(zipCode: Long, days: Int): ForecastList = requestToSources {
