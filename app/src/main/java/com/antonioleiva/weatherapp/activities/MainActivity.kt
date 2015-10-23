c4e7806a8f0af7e61a25c0f8e39ac402f88c1324
Antonio Leiva <antonioleivag@gmail.com>
2015-10-23 20:22:33 +0200

    Chapter 5: Writing your first class

---
+++ app/src/main/java/com/antonioleiva/weatherapp/activities/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.antonioleiva.weatherapp.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import com.antonioleiva.weatherapp.R
+import com.antonioleiva.weatherapp.adapters.ForecastListAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    private val items = listOf(
+            "Mon 6/23 - Sunny - 31/17",
+            "Tue 6/24 - Foggy - 21/8",
+            "Wed 6/25 - Cloudy - 22/17",
+            "Thurs 6/26 - Rainy - 18/11",
+            "Fri 6/27 - Foggy - 21/10",
+            "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+            "Sun 6/29 - Sunny - 20/7"
+    )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forecastList = findViewById(R.id.forecast_list) as RecyclerView
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+    }
+
+}
