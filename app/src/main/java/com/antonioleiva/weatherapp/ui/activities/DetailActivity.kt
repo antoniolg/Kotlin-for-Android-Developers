ded98fa6b2438df0fb18d90483b3172f9a58e016
Antonio Leiva <antonioleivag@gmail.com>
2017-06-01 08:20:58 +0200

    Chapter 27: Coroutines

--- app/src/main/java/com/antonioleiva/weatherapp/ui/activities/DetailActivity.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/ui/activities/DetailActivity.kt
@@ -12,10 +12,11 @@ import com.antonioleiva.weatherapp.extensions.textColor
 import com.antonioleiva.weatherapp.extensions.toDateString
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import org.jetbrains.anko.coroutines.experimental.bg
 import org.jetbrains.anko.ctx
-import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
-import org.jetbrains.anko.uiThread
 import java.text.DateFormat
 
 class DetailActivity : AppCompatActivity(), ToolbarManager {
@@ -35,9 +36,9 @@ class DetailActivity : AppCompatActivity(), ToolbarManager {
         toolbarTitle = intent.getStringExtra(CITY_NAME)
         enableHomeAsUp { onBackPressed() }
 
-        doAsync {
-            val result = RequestDayForecastCommand(intent.getLongExtra(ID, -1)).execute()
-            uiThread { bindForecast(result) }
+        async(UI) {
+            val result = bg { RequestDayForecastCommand(intent.getLongExtra(ID, -1)).execute() }
+            bindForecast(result.await())
         }
     }
 
