c290ebc8e6a2b82cacc00af18a9d460f46329404
Antonio Leiva <antonioleivag@gmail.com>
2017-11-27 09:41:30 +0100

    Fixes to make it work after API changes and AS 3.0 release.

--- app/src/main/java/com/antonioleiva/weatherapp/data/server/ForecastByZipCodeRequest.kt
+++ app/src/main/java/com/antonioleiva/weatherapp/data/server/ForecastByZipCodeRequest.kt
@@ -8,7 +8,7 @@ class ForecastByZipCodeRequest(private val zipCode: Long, val gson: Gson = Gson(
     companion object {
         private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
         private val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=7"
-        private val COMPLETE_URL = "$URL&APPID=$APP_ID&q="
+        private val COMPLETE_URL = "$URL&APPID=$APP_ID&zip="
     }
 
     fun execute(): ForecastResult {
